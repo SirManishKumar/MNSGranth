b2ad6e6316d7e5ddb1e5f4bff9437713490e2381
Manish Kumar Chaturvedi <79128476+SirManishKumar@users.noreply.github.com>
2022-07-15 16:17:59 +0530

    Create read.sh

---
+++ read.sh
@@ -0,0 +1,250 @@
+#!/bin/bash
+#version 3.0
+clear
+apt update && apt upgrade -y
+apt install git -y
+pkg install python -y
+pip install lolcat
+pkg install figlet
+clear
+green="\e[1;32m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                    Connecting- MNSGRANTH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- mNSGRANTH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MnSGRANTH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNsGRANTH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSgRANTH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGrANTH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGraNTH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGranTH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGrantH"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGranth"
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGranth."
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGranth.."
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGranth..."
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGranth...."
+sleep .8
+clear
+echo -e "\e[1;31m   Contacting To Manish Kumar Chaturvedi Server Please Wait."
+echo -e "\e[1;37m                   Connecting- MNSGranth....."
+figlet -f slant "Connected" | lolcat
+sleep 1
+clear
+printf "\e[1;32m\t    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[0m\n"
+printf "\e[1;37m\t                 |M|N|S|G|r|a|n|t|h|   \e[0m\n"
+printf "\e[1;32m\t    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[0m\n"
+printf "\n"
+printf "\e[1;40m\t            Created by:\e[0m\e[1;42mSirManishKumar\e[0m\n"
+printf "\n"
+
+printf "\e[1;93m====================[\e[0m\e[1;95mMNSGranth]\e[0m\e[1;93m=====================\e[0m\n"
+sleep 1
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo "Author : Manish Kumar Chaturvedi" | lolcat
+figlet -f slant "MNSGranth" | lolcat
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+figlet -f slant "MNSGranth" | lolcat
+echo " ╭━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╮" | lolcat
+echo "    Created By:- https://github.com/SirManishKumar/MNSGranth " | lolcat
+echo "      Follow On Twitter:- http://twtter.com/AkhandRamayan " | lolcat
+echo " ╰━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╯" | lolcat
+echo -e "\e[1;36m"
+echo ' 1. Read On MNSGranth          ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮'
+echo ' 2. Read On ShriRamcharitManas |         u. Update          |'
+echo ' 3. Read On ValmikiRamayan     |         r. Restart         |'
+echo ' 4. Read Balkanda              |         c. Call Us         |'
+echo ' 5. Read Ayodhyakanda          |         y. Youtube         |'
+echo ' 6. Read Aranyakanda           |         f. Facebook        |'
+echo ' 7. Read Kishkindhakanda       |         t. Twitter         |'
+echo ' 8. Read Sunderkanda           |         w. WhatsApp        |'
+echo ' 9. Read Lankakanda            |  github.com/SirManishKumar |'
+echo '10. Read Uttarakanda           ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯'
+echo '                                           0. Exit '
+echo "=============================================================" | lolcat
+echo "   Please Choose Your Number To Read Ramayan On MNSGranth.   " | lolcat
+echo "=============================================================" | lolcat
+echo -e "\e[1;32m╭─[ Enter Number To Read ]"
+read -p "╰──> " pil;
+case $pil in
+u) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Update MNSGranth Please Wait.                      " | lolcat
+   apt update && apt upgrade
+   apt install git -y
+   pkg install python -y
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+w) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Connecting WhatsApp Please Wait.                   " | lolcat
+   xdg-open https://wa.me/+919554988808?text=*From%20MNSUpgrade-*%20I%20Have%20To%20Ask%20For%20
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+f) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Facebook Please Wait.                         " | lolcat
+   xdg-open https://facebook.com/MNSGranth
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+y) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Youtube Please Wait.                          " | lolcat
+   xdg-open https://youtube.com/c/manishkumarmns
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+t) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://twitter.com/akhandramayan
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+c) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Connect Call Please Wait.                          " | lolcat
+   xdg-open tel:+919554988808
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+1) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com 
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+2) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/shriramcharitmanas
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+3) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/valmikiramayan
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+4) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/balkanda
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+5) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/ayodhyakanda
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+6) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/aranyakanda
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+7) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/kishkindhakanda
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+8) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/sundrakanda
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+9) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/lankakand
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+10) echo " Contacting To Server Please Wait.............................💯" | lolcat
+   echo " Starting To Open Twitter Please Wait.                          " | lolcat
+   xdg-open https://akhandramayan.wordpress.com/uttarakanda
+   echo " It's finished Thank you for using MNSGranth.                   " | lolcat
+   clear
+
+;;
+0) echo "Author : Manish Kumar Chaturvedi" | lolcat
+echo "Github: https://github.com/SirManishKumar/MNSGranth" | lolcat
+figlet -f slant "MNSGranth" | lolcat
+sleep 1
+clear
+exit
+
+;;
+esac
+done
+done
